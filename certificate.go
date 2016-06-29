8b67c178ee5f9eb6b7766aea0c71256b74721fca
Tim Schindler <tim@giantswarm.io>
2016-06-29 14:16:38 +0000

    aligned certificate request payloads

--- certificate.go
+++ certificate.go
@@ -17,10 +17,18 @@ type KeyPairResponse struct {
 // ca
 
 type CAKeyPairRequest struct {
+	// AllowedDomains represents a comma separate list of valid domain names the
+	// generated certificate authority is valid for.
+	AllowedDomains string `json:"allowed_domains"`
+
 	CommonName string `json:"common_name"`
+
+	// TTL configures the time to live for the requested certificate. This is a
+	// golang time string with the allowed units s, m and h.
+	TTL string `json:"ttl"`
 }
 
-func (c *Client) GetCAKeyPair(clusterID string, request CAKeyPairRequest) (KeyPairResponse, error) {
+func (c *Client) GetOrCreateCAKeyPair(clusterID string, request CAKeyPairRequest) (KeyPairResponse, error) {
 	resp, err := apischema.FromHTTPResponse(c.postJSON(fmt.Sprintf("/v1/cluster/%s/certificate/key-pair/ca", clusterID), request))
 	if err != nil {
 		return KeyPairResponse{}, maskAny(err)
@@ -42,11 +50,17 @@ func (c *Client) GetCAKeyPair(clusterID string, request CAKeyPairRequest) (KeyPa
 // signed
 
 type SignedKeyPairRequest struct {
-	CommonName string   `json:"common_name"`
-	IPSANs     []string `json:"ip_sans"`
+	CommonName string `json:"common_name"`
+
+	// IPSANs represents a comma separate lists of IPs.
+	IPSANs string `json:"ip_sans"`
+
+	// TTL configures the time to live for the requested certificate. This is a
+	// golang time string with the allowed units s, m and h.
+	TTL string `json:"ttl"`
 }
 
-func (c *Client) GetSignedKeyPair(clusterID, certName string, request SignedKeyPairRequest) (KeyPairResponse, error) {
+func (c *Client) GetOrCreateSignedKeyPair(clusterID, certName string, request SignedKeyPairRequest) (KeyPairResponse, error) {
 	resp, err := apischema.FromHTTPResponse(c.postJSON(fmt.Sprintf("/v1/cluster/%s/certificate/key-pair/%s", clusterID, certName), request))
 	if err != nil {
 		return KeyPairResponse{}, maskAny(err)

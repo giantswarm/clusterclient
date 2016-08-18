73f5dd25099f6c009c75ce875d34db860e000cc4
Tim Schindler <xh3b4sd@gmail.com>
2016-08-18 17:15:29 +0200

    added serial number to key pair creation output (#6)
    * added serial number to key pair creation output

---
+++ key_pair.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/giantswarm/api-schema"
+)
+
+type CreateKeyPairRequest struct {
+	Description string `json:"description"`
+}
+
+type CreateKeyPairResponse struct {
+	CertificateAuthorityData string    `json:"certificate_authority_data"`
+	ClientKeyData            string    `json:"client_key_data"`
+	ClientCertificateData    string    `json:"client_certificate_data"`
+	CreateDate               time.Time `json:"create_date"`
+	Description              string    `json:"description"`
+	SerialNumber             string    `json:"serial_number"`
+}
+
+func (c *Client) CreateClusterKeyPair(clusterID string, request CreateKeyPairRequest) (CreateKeyPairResponse, error) {
+	resp, err := apischema.FromHTTPResponse(c.postJSON(fmt.Sprintf("/v1/cluster/%s/key-pair", clusterID), request))
+	if err != nil {
+		return CreateKeyPairResponse{}, maskAny(err)
+	}
+
+	if err := resp.EnsureStatusCodes(apischema.STATUS_CODE_DATA); err != nil {
+		return CreateKeyPairResponse{}, mapError(maskAny(err))
+	}
+
+	var response CreateKeyPairResponse
+
+	if err := resp.UnmarshalData(&response); err != nil {
+		return CreateKeyPairResponse{}, maskAny(err)
+	}
+
+	return response, nil
+}

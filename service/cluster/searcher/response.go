531eb5f30949992a717174c8113286d004d2f0cc
Tim Schindler <xh3b4sd@gmail.com>
2016-12-04 01:46:43 +0100

    extended the cluster object response of the searcher as specified by the mock API

--- service/cluster/searcher/response.go
+++ service/cluster/searcher/response.go
@@ -1,6 +1,10 @@
 package searcher
 
-import "time"
+import (
+	"time"
+
+	"github.com/giantswarm/clusterclient/service/cluster/searcher/config"
+)
 
 // Response is the return value of the service action.
 type Response struct {
@@ -8,14 +12,28 @@ type Response struct {
 	CreateDate  time.Time `json:"create_date"`
 	ID          string    `json:"id"`
 	Name        string    `json:"name"`
+
+	Owner string `json:"owner"`
+
+	KubernetesVersion string `json:"kubernetes_version"`
+
+	Masters []*config.Master `json:"masters"`
+	Workers []*config.Worker `json:"workers"`
 }
 
-// DefaultResponse provides a default response by best effort.
+// DefaultResponse provides a default response object by best effort.
 func DefaultResponse() *Response {
 	return &Response{
 		APIEndpoint: "",
 		CreateDate:  time.Time{},
 		ID:          "",
 		Name:        "",
+
+		Owner: "",
+
+		KubernetesVersion: "",
+
+		Masters: []*config.Master{},
+		Workers: []*config.Worker{},
 	}
 }

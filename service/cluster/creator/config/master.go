f672a50befbc6d08d4acf3643a1317a9d2c88b5d
Tim Schindler <xh3b4sd@gmail.com>
2016-12-07 12:12:47 +0100

    removed labels from master config

--- service/cluster/creator/config/master.go
+++ service/cluster/creator/config/master.go
@@ -2,17 +2,15 @@ package config
 
 // Master configures the Kubernetes master nodes.
 type Master struct {
-	CPU     *CPU              `json:"cpu"`
-	Labels  map[string]string `json:"labels"`
-	Ram     *Ram              `json:"ram"`
-	Storage *Storage          `json:"storage"`
+	CPU     *CPU     `json:"cpu"`
+	Ram     *Ram     `json:"ram"`
+	Storage *Storage `json:"storage"`
 }
 
 // DefaultMaster provides a default master configuration by best effort.
 func DefaultMaster() *Master {
 	return &Master{
 		CPU:     DefaultCPU(),
-		Labels:  map[string]string{},
 		Ram:     DefaultRam(),
 		Storage: DefaultStorage(),
 	}

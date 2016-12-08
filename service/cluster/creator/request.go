b278c03b8b81b82630cc7f3cbc53af4bc5852383
Tim Schindler <xh3b4sd@gmail.com>
2016-12-08 11:39:16 +0100

    v3 compatibility

--- service/cluster/creator/request.go
+++ service/cluster/creator/request.go
@@ -1,9 +1,19 @@
 package creator
 
-import "github.com/giantswarm/clusterclient/service/cluster/creator/config"
+import (
+	"time"
+
+	"github.com/giantswarm/clusterclient/service/cluster/creator/config"
+)
 
 // Request is the configuration for the service action.
 type Request struct {
+	// TODO remove this when v3 is no more used
+	APIEndpoint string    `json:"api_endpoint"`
+	CreateDate  time.Time `json:"create_date"`
+	ID          string    `json:"id"`
+	// TODO remove this when v3 is no more used
+
 	Name string `json:"name,omitempty"`
 
 	Owner string `json:"owner,omitempty"`

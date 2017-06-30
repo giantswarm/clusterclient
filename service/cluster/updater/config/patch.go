92fe7d93ff8adab31daa877d0ccc63fcc506d4a1
Ross Fairbanks <ross@giantswarm.io>
2017-06-30 09:16:44 +0200

    Add support for patching worker nodes

--- service/cluster/updater/config/patch.go
+++ service/cluster/updater/config/patch.go
@@ -2,12 +2,14 @@ package config
 
 // Patch is the cluster specific configuration.
 type Patch struct {
-	Owner string `json:"owner"`
+	Owner   string   `json:"owner"`
+	Workers []Worker `json:"workers,omitempty"`
 }
 
 // DefaultPatch provides a default patch by best effort.
 func DefaultPatch() Patch {
 	return Patch{
-		Owner: "",
+		Owner:   "",
+		Workers: []Worker{},
 	}
 }

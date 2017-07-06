3c23720a7956b65979e4813dc000385e76fd52d1
Marian Steinbach <marian@giantswarm.io>
2017-07-06 17:09:36 +0200

    Moved PatchAttributes into Patch

--- service/cluster/updater/config/cluster.go
+++ service/cluster/updater/config/cluster.go
@@ -2,16 +2,14 @@ package config
 
 // Cluster is the cluster specific configuration.
 type Cluster struct {
-	ID              string `json:"id"`
-	Patch           Patch  `json:"patch"`
-	PatchAttributes []string
+	ID    string `json:"id"`
+	Patch Patch  `json:"patch"`
 }
 
 // DefaultCluster provides a default cluster by best effort.
 func DefaultCluster() Cluster {
 	return Cluster{
-		ID:              "",
-		Patch:           DefaultPatch(),
-		PatchAttributes: []string{},
+		ID:    "",
+		Patch: DefaultPatch(),
 	}
 }
--- service/cluster/updater/config/patch.go
+++ service/cluster/updater/config/patch.go
@@ -2,14 +2,16 @@ package config
 
 // Patch is the cluster specific configuration.
 type Patch struct {
-	Owner   string        `json:"owner"`
-	Workers []interface{} `json:"workers"`
+	Owner           string        `json:"owner"`
+	Workers         []interface{} `json:"workers"`
+	PatchAttributes []string      `json:"patch_attributes"`
 }
 
 // DefaultPatch provides a default patch by best effort.
 func DefaultPatch() Patch {
 	return Patch{
-		Owner:   "",
-		Workers: make([]interface{}, 0),
+		Owner:           "",
+		Workers:         make([]interface{}, 0),
+		PatchAttributes: []string{},
 	}
 }

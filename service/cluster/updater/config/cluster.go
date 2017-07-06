88f91813828d46b1b45e3031a550aa23c0686006
Marian Steinbach <marian@giantswarm.io>
2017-07-06 16:33:52 +0200

    Adding PatchAttributes and Workers fields

--- service/cluster/updater/config/cluster.go
+++ service/cluster/updater/config/cluster.go
@@ -2,14 +2,16 @@ package config
 
 // Cluster is the cluster specific configuration.
 type Cluster struct {
-	ID    string `json:"id"`
-	Patch Patch  `json:"patch"`
+	ID              string `json:"id"`
+	Patch           Patch  `json:"patch"`
+	PatchAttributes []string
 }
 
 // DefaultCluster provides a default cluster by best effort.
 func DefaultCluster() Cluster {
 	return Cluster{
-		ID:    "",
-		Patch: DefaultPatch(),
+		ID:              "",
+		Patch:           DefaultPatch(),
+		PatchAttributes: []string{},
 	}
 }

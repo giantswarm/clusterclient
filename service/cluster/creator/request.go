243b367454f8274c61c8cd61a9dfc0ceea59c0a7
Tim Schindler <xh3b4sd@gmail.com>
2017-11-10 15:25:30 +0100

    added version bundles to cluster creator request

--- service/cluster/creator/request.go
+++ service/cluster/creator/request.go
@@ -3,17 +3,19 @@ package creator
 import (
 	"github.com/giantswarm/clusterclient/service/cluster/creator/request"
 	"github.com/giantswarm/clusterclient/service/cluster/creator/request/aws"
+	"github.com/giantswarm/versionbundle"
 )
 
 // Request is the configuration for the service action.
 type Request struct {
-	AWS               aws.Cluster      `json:"aws,omitempty"`
-	KubernetesVersion string           `json:"kubernetes_version,omitempty"`
-	Masters           []request.Master `json:"masters,omitempty"`
-	Name              string           `json:"name,omitempty"`
-	Owner             string           `json:"owner,omitempty"`
-	ReleaseVersion    string           `json:"release_version,omitempty"`
-	Workers           []request.Worker `json:"workers,omitempty"`
+	AWS               aws.Cluster            `json:"aws,omitempty"`
+	KubernetesVersion string                 `json:"kubernetes_version,omitempty"`
+	Masters           []request.Master       `json:"masters,omitempty"`
+	Name              string                 `json:"name,omitempty"`
+	Owner             string                 `json:"owner,omitempty"`
+	ReleaseVersion    string                 `json:"release_version,omitempty"`
+	VersionBundles    []versionbundle.Bundle `json:"version_bundle,omitempty"`
+	Workers           []request.Worker       `json:"workers,omitempty"`
 }
 
 // DefaultRequest provides a default request object by best effort.
@@ -25,6 +27,7 @@ func DefaultRequest() Request {
 		Name:              "",
 		Owner:             "",
 		ReleaseVersion:    "",
+		VersionBundles:    []versionbundle.Bundle{},
 		Workers:           []request.Worker{},
 	}
 }

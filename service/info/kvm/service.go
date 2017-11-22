96aaa533849b8e40c63cf18b8c4b8bb6c578144c
Oliver Ponder <oliver.ponder@gmail.com>
2017-11-22 18:41:47 +0100

    Add methods for accessing kvm and aws endpoints.

---
+++ service/info/kvm/service.go
@@ -0,0 +1,91 @@
+package kvm
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/go-resty/resty"
+	"golang.org/x/net/context"
+)
+
+const (
+	// Endpoint is the API endpoint of the service this client action interacts
+	// with.
+	Endpoint = "/v1/info/kvm/"
+	// Name is the service name being implemented. This can be used for e.g.
+	// logging.
+	Name = "info/kvm"
+)
+
+// Config represents the configuration used to create a lister service.
+type Config struct {
+	// Dependencies.
+	Logger     micrologger.Logger
+	RestClient *resty.Client
+
+	// Settings.
+	URL *url.URL
+}
+
+// DefaultConfig provides a default configuration to create a new lister
+// service by best effort.
+func DefaultConfig() Config {
+	newConfig := Config{
+		// Dependencies.
+		Logger:     nil,
+		RestClient: resty.New(),
+
+		// Settings.
+		URL: nil,
+	}
+
+	return newConfig
+}
+
+// New creates a new configured lister service.
+func New(config Config) (*Service, error) {
+	// Dependencies.
+	if config.RestClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "rest client must not be empty")
+	}
+
+	// Settings.
+	if config.URL == nil {
+		return nil, microerror.Maskf(invalidConfigError, "URL must not be empty")
+	}
+
+	newService := &Service{
+		Config: config,
+	}
+
+	return newService, nil
+}
+
+type Service struct {
+	Config
+}
+
+func (s *Service) List(ctx context.Context) (*Response, error) {
+	u, err := s.URL.Parse(Endpoint)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	s.Logger.Log("debug", fmt.Sprintf("sending GET request to %s", u.String()), "service", Name)
+	r, err := s.RestClient.R().SetResult(DefaultResponse()).Get(u.String())
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	s.Logger.Log("debug", fmt.Sprintf("received status code %d", r.StatusCode()), "service", Name)
+
+	if r.StatusCode() != http.StatusOK {
+		return nil, microerror.Mask(fmt.Errorf(string(r.Body())))
+	}
+
+	response := r.Result().(*Response)
+
+	return response, nil
+}

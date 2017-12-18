019730455122598efb65b6e07be4d6e5b1e04a83
Tim Schindler <xh3b4sd@gmail.com>
2017-12-18 11:33:45 +0100

    added release version to cluster updater (#46)

--- service/cluster/updater/config/patch.go
+++ service/cluster/updater/config/patch.go
@@ -2,16 +2,18 @@ package config
 
 // Patch is the cluster specific configuration.
 type Patch struct {
-	Name    string   `json:"name"`
-	Owner   string   `json:"owner"`
-	Workers []Worker `json:"workers,omitempty"`
+	Name           string   `json:"name"`
+	Owner          string   `json:"owner"`
+	ReleaseVersion string   `json:"release_version,omitempty"`
+	Workers        []Worker `json:"workers,omitempty"`
 }
 
 // DefaultPatch provides a default patch by best effort.
 func DefaultPatch() Patch {
 	return Patch{
-		Name:    "",
-		Owner:   "",
-		Workers: []Worker{},
+		Name:           "",
+		Owner:          "",
+		ReleaseVersion: "",
+		Workers:        []Worker{},
 	}
 }

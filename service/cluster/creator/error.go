519ef8982b8f541aad870e290f17d889a521cce8
Tim Schindler <xh3b4sd@gmail.com>
2018-07-31 21:34:45 +0200

    rewrite errors (#64)

--- service/cluster/creator/error.go
+++ service/cluster/creator/error.go
@@ -4,21 +4,27 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var invalidConfigError = microerror.New("invalid config")
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
-var invalidRequestError = microerror.New("invalid request")
+var invalidRequestError = &microerror.Error{
+	Kind: "invalidRequestError",
+}
 
 // IsInvalidRequest asserts invalidRequestError.
 func IsInvalidRequest(err error) bool {
 	return microerror.Cause(err) == invalidRequestError
 }
 
-var notFoundError = microerror.New("not found")
+var notFoundError = &microerror.Error{
+	Kind: "notFoundError",
+}
 
 // IsNotFound asserts notFoundError.
 func IsNotFound(err error) bool {

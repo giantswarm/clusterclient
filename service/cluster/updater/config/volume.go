00514dec55511d5b9453f3a5297676750d86fd6e
Timo Derstappen <teemow@gmail.com>
2018-09-05 19:18:19 +0200

    typos.

--- service/cluster/updater/config/volume.go
+++ service/cluster/updater/config/volume.go
@@ -1,16 +1,16 @@
 package config
 
-// Volumes defines all disks attached to the node
+// Volumes defines all disks attached to the node.
 type Volumes struct {
 	Docker Volume `json:"docker"`
 }
 
-// Volume defines a disk attached to the node
+// Volume defines a disk attached to the node.
 type Volume struct {
 	SizeGB float64 `json:"size_gb"`
 }
 
-// DefaultVolumes provides a default volumes configuration for the nodes
+// DefaultVolumes provides a default volumes configuration for the nodes.
 func DefaultVolumes() Volumes {
 	return Volumes{
 		Docker: Volume{

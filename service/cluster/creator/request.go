6ebd7876d2ab15fe1d592ca52a67c9410c6363c7
Tim Schindler <xh3b4sd@gmail.com>
2018-09-20 17:34:08 +0200

    sends cluster ID when creating cluster (#68)

--- service/cluster/creator/request.go
+++ service/cluster/creator/request.go
@@ -9,6 +9,7 @@ import (
 // Request is the configuration for the service action.
 type Request struct {
 	AWS            aws.Cluster            `json:"aws,omitempty"`
+	ID             string                 `json:"id,omitempty"`
 	Name           string                 `json:"name,omitempty"`
 	Owner          string                 `json:"owner,omitempty"`
 	ReleaseVersion string                 `json:"release_version,omitempty"`
@@ -20,6 +21,7 @@ type Request struct {
 func DefaultRequest() Request {
 	return Request{
 		AWS:            aws.DefaultCluster(),
+		ID:             "",
 		Name:           "",
 		Owner:          "",
 		ReleaseVersion: "",

9011ed25fdfede437d18da32ad0277c1d6c2c881
MarcelMue <marcel.mueller1@rwth-aachen.de>
2018-11-22 17:03:19 +0100

    Move down in structure

--- service/cluster/searcher/response/aws/cluster.go
+++ service/cluster/searcher/response/aws/cluster.go
@@ -2,14 +2,12 @@ package aws
 
 // Cluster configures AWS-specific cluster settings.
 type Cluster struct {
-	AvailabilityZones []string          `json:"availability_zones,omitempty"`
-	ResourceTags      map[string]string `json:"resource_tags"`
+	ResourceTags map[string]string `json:"resource_tags"`
 }
 
 // DefaultCluster provides a default Cluster.
 func DefaultCluster() Cluster {
 	return Cluster{
-		AvailabilityZones: []string{},
-		ResourceTags:      map[string]string{},
+		ResourceTags: map[string]string{},
 	}
 }
